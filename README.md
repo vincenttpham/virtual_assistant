206e995bcbf99f7ba36dcad9e552c882ea563fa0
Vincent Pham <crystalpham@Crystals-MacBook-Pro.local>
2023-09-29 15:15:31 -0700

    readme edit

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 #virtual_assistant
+
+This is a Virtual Assistant app made with a python/django back end and bootstrap/javascript front end. Powered by OpenAI API, the assistant can answer your questions and even read your CSV spreadsheets through the help of python logic.
+
+Setup:
+
+1. To use this app on your local machine, make sure you have python3 installed. Setup a virtual environment in the directory of your choice with "python3 -m venv va_venv" for example.
+
+2. After creating the environment, activate it with "source va_venv/bin/activate".
+
+3. Clone the project to the directory of your choice and then move into the directory through the terminal "cd virtual_assistant". Make sure your virtual environment is activated. Once inside the project directory, install the requirements with "pip install -r requirements.txt".
+
+4. If you don't have an OpenAI API key, head over to https://platform.openai.com/account/api-keys to generate one. Once you have your key, create a new file called "secret_key.py" and add API_KEY='(your OpenAI API key)'.
+
+5. Finally, while in the project directory, run the app with "python manage.py runserver" and the app should be running locally on your machine. Remember to go into the "settings.py" file and remove the contents inside the brackets of ALLOWED_HOSTS = []. You should be able to find the app at "localhost:8000" afterwards.
+
+Deployment:
+
+To deploy this app serverless on AWS Lambda while serving the static files from an S3 bucket, I used zappa and django-s3-storage.
+
+Instructions:
+
+1. While virtual environment is activated and inside the project directory, run "pip install zappa".
+
+2. Once installed, run "zappa init". This will create a "zappa_settings.json" file. Add, "slim_handler": true, to the json file. This allows the sqlite database to be used with your lambda processes by serving it from an Amazon S3 bucket.
+
+3. Once configured, run "zappa deploy" and it should output the link to the project.
+
+4. After deployment, go into the "settings.py" file and add the address to "ALLOWED_HOSTS = []".
+
+5. If you want to server static files from Amazon S3 to the project add these lines to the bottom of your "settings.py" file.
+
+
+
+YOUR_S3_BUCKET = 'virtual-assistant-static'
+
+STATICFILES_STORAGE = 'django_s3_storage.storage.StaticS3Storage'
+AWS_S3_BUCKET_NAME_STATIC = YOUR_S3_BUCKET
+
+# These next two lines will serve the static files directly 
+# from the s3 bucket
+AWS_S3_CUSTOM_DOMAIN = f'{YOUR_S3_BUCKET}.s3.amazonaws.com'
+
+STATIC_LOCATION = 'static'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+
+AWS_DEFAULT_ACL = None
+
+
+
+Where it says YOUR_S3_BUCKET = 'virtual-assistant-static', replace "virtual-assistant-static" with the name of your Amazon S3 bucket after you've created it.
+
+6. Run "python manage.py collectstatic" to upload the static files to your S3 bucket to have them served to your Lambda process.
+
+
+
+And just like that, the project is deployed and ready for use.
--- virtual_assistant_app/views.py
+++ virtual_assistant_app/views.py
@@ -12,10 +12,17 @@ def index(request):
             request.session['messages'] = [
                 {"role": "assistant", "content": "Hi, how can I help you?"},
             ]
+        # messages will be used for display on the chatbox
         if 'prompts' not in request.session:
             request.session['prompts'] = [
                 {"role": "system", "content": ""},
             ]
+        # prompts will be used for chat completions
+
+        # this is because if a file is uploaded with a prompt, the below logic will parse through the data and format it into the prompt
+
+        # by keeping 2 separate logs, this keeps the app user from seeing uploaded file data on their chatbox
+
         if request.method == 'POST':
             # get the prompt from the form
             prompt = request.POST.get('prompt')
@@ -24,8 +31,10 @@ def index(request):
             request.session['chatbox'] = 'showbox'
             if 'upload' in request.FILES:
                 upload = request.FILES['upload']
+                # app user will see the file name instead of its data
                 request.session['messages'].append({"role": "user", "content": upload.name})
                 request.session.modified = True
+                # if upload has no prompt, return message
                 if prompt == '':
                     request.session['messages'].append({"role": "assistant", "content": "If you send me a file without telling me what to do with it, how am I supposed to help you?"})
                     request.session.modified = True
@@ -36,6 +45,7 @@ def index(request):
                         'showbox': request.session['chatbox'],
                     }
                     return render(request, 'index.html', context)
+                # if uploaded file isnt csv, return message
                 if not upload.name.endswith('.csv'):
                     request.session['messages'].append({"role": "assistant", "content": "Sorry, I'm only programmed to read CSV files at the moment. Try sending me a CSV file and I'll try my best to assist you."})
                     request.session.modified = True
@@ -63,7 +73,7 @@ def index(request):
                 lines = file_data.split("\n")
                 next(iter(lines))
 
-                #loop over the lines and add them in prompt. If error , store as string and then display
+                # loop over the lines and add them to prompt
                 prompt += "\n"
                 upload_content = ""
                 for line in lines:
@@ -73,7 +83,8 @@ def index(request):
                     upload_content += "\n"
                 prompt += f"###\n{upload_content}\n###"
 
-            # get the temperature from the form
+            # if temperature is used, get the temperature from the form
+            # I decided to remove adjustable temerature from the template for simplicity
             temperature = float(request.POST.get('temperature', 0.1))
             # append the prompt to the messages list
             request.session['messages'].append({"role": "user", "content": message})
@@ -81,7 +92,7 @@ def index(request):
             request.session['prompts'].append({"role": "user", "content": prompt})
             # set the session as modified
             request.session.modified = True
-            # call the openai API
+            # call the OpenAI API
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=request.session['prompts'],
@@ -93,9 +104,10 @@ def index(request):
             # append the response to the messages list
             request.session['messages'].append({"role": "assistant", "content": formatted_response})
             request.session.modified = True
+            # append the response to the prompts list
             request.session['prompts'].append({"role": "assistant", "content": formatted_response})
             request.session.modified = True
-            # redirect to the home page
+            # redirect to the home page with new messages displayed
             context = {
                 'messages': request.session['messages'],
                 'prompt': '',
