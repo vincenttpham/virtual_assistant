d0cad57687d35603613727ce519b73ff7bfe3fcb
Vincent Pham <crystalpham@Crystals-MacBook-Pro.local>
2023-10-11 17:06:52 -0700

    code cleanup/more comments

--- va/views.py
+++ va/views.py
@@ -8,23 +8,30 @@ openai.api_key = API_KEY
 
 # Create your views here.
 def index(request):
-    # if the session does not have a messages key, create one
+    # if session has no dicts, create them
+
     # messages will be used for display on the chatbox
     if 'messages' not in request.session:
         request.session['messages'] = [
             {"role": "assistant", "content": "Hi, how can I help you?"},
         ]
-    messages = request.session['messages']
+    
     # prompts will be used for chat completions
     if 'prompts' not in request.session:
         request.session['prompts'] = []
-    prompts = request.session['prompts']
-    # by keeping 2 separate logs, I can put custom responses into the message view while keeping the token count low
+    
+    # by keeping 2 separate logs, I can put custom responses into the message view while keeping the token count low - see examples in send_prompt
 
-    # set persistent chatbox for mobile screens
+    # chatbox key used for persistent message view on mobile
     if 'chatbox' not in request.session:
         request.session['chatbox'] = ''
+    
+    # assign values to session dicts for context rendering
+    messages = request.session['messages']
+    prompts = request.session['prompts']
     chatbox = request.session['chatbox']
+
+    # prompts don't need rendering
     context = {
             'messages': messages,
             'showbox': chatbox,
@@ -36,36 +43,44 @@ def send_prompt(request):
         if request.method == 'POST':
             # get the prompt from the form
             prompt = request.POST.get('prompt')
+            # separate value for prompt to keep file upload data away from user view
             message = request.POST.get('prompt')
             # set persistent chatbox for mobile screens
             request.session['chatbox'] = 'showbox'
+
             if 'upload' in request.FILES:
                 upload = request.FILES['upload']
                 # user will see file name to confirm successful upload
                 request.session['messages'].append({"role": "user", "content": upload.name})
                 request.session.modified = True
+
                 # if no prompt
                 if prompt == '':
                     request.session['messages'].append({"role": "assistant", "content": "If you send me a file without instructions on what to do with it, how am I supposed to help you?"})
+                    # save session modifications
                     request.session.modified = True
                     return redirect('index')
+                
                 # if uploaded file isnt csv, return message
                 if not upload.name.endswith('.csv'):
                     request.session['messages'].append({"role": "assistant", "content": "Sorry, I'm only programmed to read CSV files at the moment. Try sending me a CSV file and I'll try my best to assist you."})
+                    # save session modifications
                     request.session.modified = True
                     return redirect('index')
+                
                 #if file is too large, return
                 if upload.multiple_chunks():
                     request.session['messages'].append({"role": "assistant", "content": "The uploaded file is too big (%.2f MB)."})
+                    # save session modifications
                     request.session.modified = True
                     return redirect('index')
 
-                # add file data to session
+                # add file data to prompt
                 file_data = upload.read().decode("unicode_escape")
                 lines = file_data.split("\n")
                 next(iter(lines))
-                # loop over the lines and add them to prompt
                 upload_content = ""
+                # loop over the lines and add them to prompt
                 for line in lines:
                     upload_content += str(f"{line}")
                     fields = line.split(",")
@@ -73,7 +88,7 @@ def send_prompt(request):
                         upload_content += str(f"{field}")
                 prompt += f"###{upload_content}###"
 
-                # testing potential file reading in session for lower token count
+                # ignore - testing potential file data logging in session for lower token count
                 """
                 request.session['upload'] = []
                 reader = csv.DictReader(upload.read().decode('unicode_escape').splitlines(), delimiter=',')
@@ -81,9 +96,12 @@ def send_prompt(request):
                     request.session['upload'].append(row)
                     request.session.modified = True
                 """
+            
+            # append prompt to messages view and prompt
+            request.session['messages'].append({"role": "user", "content": message})
+            request.session['prompts'].append({"role": "user", "content": prompt})
 
-            # s_key = request.session.session_key
-
+            # define functions for chat completion
             functions = [
                 {
                     "name": "send_email",
@@ -114,10 +132,6 @@ def send_prompt(request):
                 },
             ]
 
-            # append the prompt to the messages list
-            request.session['messages'].append({"role": "user", "content": message})
-            request.session['prompts'].append({"role": "user", "content": prompt})
-
             # Step 1: send the conversation and available functions to GPT
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
@@ -138,16 +152,21 @@ def send_prompt(request):
                 function_name = response_message["function_call"]["name"]
                 function_to_call = available_functions[function_name]
                 function_args = json.loads(response_message["function_call"]["arguments"])
+
+                # check which function GPT wants to call to assign different arguments
                 if function_name == "send_email":
                     function_response = function_to_call(
                         recipients=function_args.get("recipients"),
                         subject=function_args.get("subject"),
                         body=function_args.get("body"),
                     )
+                # original user prompt used as argument for image generation
                 if function_name == "generate_image":
                     function_response = function_to_call(
                         prompt=prompt,
                     )
+                
+                # append function response to messages view and prompt
                 request.session['messages'].append(
                     {
                         "role": "function",
@@ -162,12 +181,14 @@ def send_prompt(request):
                         "content": function_response,
                     }
                 )
+                # save session modifications
                 request.session.modified = True
                 return redirect('index')
             
-            # append the response to the messages list
+            # append response to messages view and prompt
             request.session['messages'].append(response_message)
             request.session['prompts'].append(response_message)
+            # save session modifications
             request.session.modified = True
             return redirect('index')
         
@@ -178,11 +199,13 @@ def send_prompt(request):
     except Exception as e:
         # if there is an error, return error message
         request.session['messages'].append({"role": "assistant", "content": str(e)})
+        # save session modifications
         request.session.modified = True
         return redirect('index')
 
 def send_email(recipients, subject, body):
     try:
+        # create email with parameters passed from function call
         email = EmailMessage(
             subject,
             body,
@@ -191,6 +214,7 @@ def send_email(recipients, subject, body):
             reply_to=[settings.EMAIL_HOST_USER],
             headers={'Message-ID': 'foo'},
         )
+        # send email and return success message with draft
         email.send(fail_silently=False)
         success = f"Email has been sent successfully.\n\nTo: {recipients}\nSubject: {subject}\nBody:\n\n{body}"
         return success
@@ -198,14 +222,20 @@ def send_email(recipients, subject, body):
         return e
 
 def generate_image(prompt):
-    response = openai.Image.create(
-        prompt=prompt,
-        n=1,
-        size="256x256"
-    )
-    image_url = response['data'][0]['url']
-    return image_url
+    try:
+        # create image with prompt passed from function call
+        response = openai.Image.create(
+            prompt=prompt,
+            n=1,
+            size="256x256"
+        )
+        image_url = response['data'][0]['url']
+        # return image url to be displayed in template
+        return image_url
+    except Exception as e:
+        return e
 
+# ignore - testing
 """
 def personalized_email_outreach(s_key, message):
     s = Session.objects.get(pk=s_key)
@@ -230,7 +260,7 @@ def personalized_email_outreach(s_key, message):
 """
 
 def new_chat(request):
-    # clear the messages list
+    # clear session to start new chat
     request.session.pop('messages', None)
     request.session.pop('prompts', None)
     request.session.pop('upload', None)
--- virtual_assistant/settings.py
+++ virtual_assistant/settings.py
@@ -22,14 +22,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-mr6zfr6y0&z(qpby7=b0z3o6rj1e6$n%24p8(2cbovxhq&s-m_'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = '587'
-EMAIL_HOST_USER = 'vincenttpham@gmail.com'
-EMAIL_HOST_PASSWORD = 'bhfzoujezsoxtxuk'
-EMAIL_USE_TLS = True
-EMAIL_USE_SSL = False
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -140,3 +132,13 @@ AWS_S3_CUSTOM_DOMAIN = f'{YOUR_S3_BUCKET}.s3.amazonaws.com'
 STATIC_LOCATION = 'static'
 STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 AWS_DEFAULT_ACL = None
+
+# Email backend for sending emails
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = 'vincenttpham@gmail.com'
+EMAIL_HOST_PASSWORD = 'bhfzoujezsoxtxuk'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
