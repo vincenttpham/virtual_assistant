7048c339086dfd8b321da7415e96e547649d9ad9
Vincent Pham <crystalpham@Crystals-MacBook-Pro.local>
2023-10-12 19:16:06 -0700

    code cleanup

--- va/views.py
+++ va/views.py
@@ -39,105 +39,106 @@ def index(request):
     return render(request, 'index.html', context)
 
 def send_prompt(request):
-    try:
-        if request.method == 'POST':
-            # get the prompt from the form
-            prompt = request.POST.get('prompt')
-            # separate value for prompt to keep file upload data away from user view
-            message = request.POST.get('prompt')
-            # set persistent chatbox for mobile screens
-            request.session['chatbox'] = 'showbox'
+    if request.method == 'POST':
+        # get the prompt from the form
+        prompt = request.POST.get('prompt')
+        # separate value for prompt to keep file upload data away from user view
+        message = request.POST.get('prompt')
+        # set persistent chatbox for mobile screens
+        request.session['chatbox'] = 'showbox'
 
-            if 'upload' in request.FILES:
-                upload = request.FILES['upload']
-                # user will see file name to confirm successful upload
-                request.session['messages'].append({"role": "user", "content": upload.name})
+        if 'upload' in request.FILES:
+            upload = request.FILES['upload']
+            # user will see file name to confirm successful upload
+            request.session['messages'].append({"role": "user", "content": upload.name})
+            request.session.modified = True
+
+            # if no prompt
+            if prompt == '':
+                request.session['messages'].append({"role": "assistant", "content": "If you send me a file without instructions on what to do with it, how am I supposed to help you?"})
+                # save session modifications
                 request.session.modified = True
-
-                # if no prompt
-                if prompt == '':
-                    request.session['messages'].append({"role": "assistant", "content": "If you send me a file without instructions on what to do with it, how am I supposed to help you?"})
-                    # save session modifications
-                    request.session.modified = True
-                    return redirect('index')
-                
-                # if uploaded file isnt csv, return message
-                if not upload.name.endswith('.csv'):
-                    request.session['messages'].append({"role": "assistant", "content": "Sorry, I'm only programmed to read CSV files at the moment. Try sending me a CSV file and I'll try my best to assist you."})
-                    # save session modifications
-                    request.session.modified = True
-                    return redirect('index')
-                
-                #if file is too large, return
-                if upload.multiple_chunks():
-                    request.session['messages'].append({"role": "assistant", "content": "The uploaded file is too big (%.2f MB)."})
-                    # save session modifications
-                    request.session.modified = True
-                    return redirect('index')
-
-                # add file data to prompt
-                file_data = upload.read().decode("unicode_escape")
-                lines = file_data.split("\n")
-                next(iter(lines))
-                upload_content = ""
-                # loop over the lines and add them to prompt
-                for line in lines:
-                    upload_content += str(f"{line}")
-                    fields = line.split(",")
-                    for field in fields:
-                        upload_content += str(f"{field}")
-                prompt += f"###{upload_content}###"
-
-                # ignore - testing potential file data logging in session for lower token count
-                """
-                request.session['upload'] = []
-                reader = csv.DictReader(upload.read().decode('unicode_escape').splitlines(), delimiter=',')
-                for row in reader:
-                    request.session['upload'].append(row)
-                    request.session.modified = True
-                """
+                return redirect('index')
             
-            # append prompt to messages view and prompt
-            request.session['messages'].append({"role": "user", "content": message})
-            request.session['prompts'].append({"role": "user", "content": prompt})
+            # if uploaded file isnt csv, return message
+            if not upload.name.endswith('.csv'):
+                request.session['messages'].append({"role": "assistant", "content": "Sorry, I'm only programmed to read CSV files at the moment. Try sending me a CSV file and I'll try my best to assist you."})
+                # save session modifications
+                request.session.modified = True
+                return redirect('index')
+            
+            #if file is too large, return
+            if upload.multiple_chunks():
+                request.session['messages'].append({"role": "assistant", "content": "The uploaded file is too big (%.2f MB)."})
+                # save session modifications
+                request.session.modified = True
+                return redirect('index')
 
-            # define functions for chat completion
-            functions = [
-                {
-                    "name": "send_email",
-                    "description": "Sends an email to the specified recipients",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {
-                            "recipients": {
-                                "type": "array",
-                                "items": {
-                                    "type": "string",
-                                },
-                                "description": "Email addresses to send the email to",
+            # add file data to prompt
+            file_data = upload.read().decode("unicode_escape")
+            lines = file_data.split("\n")
+            next(iter(lines))
+            upload_content = ""
+            # loop over the lines and add them to prompt
+            for line in lines:
+                upload_content += str(f"{line}")
+                fields = line.split(",")
+                for field in fields:
+                    upload_content += str(f"{field}")
+            prompt += f"###{upload_content}###"
+
+            # ignore - testing potential file data logging in session for lower token count
+            """
+            request.session['upload'] = []
+            reader = csv.DictReader(upload.read().decode('unicode_escape').splitlines(), delimiter=',')
+            for row in reader:
+                request.session['upload'].append(row)
+                request.session.modified = True
+            """
+        
+        # append prompt to messages view and prompt
+        request.session['messages'].append({"role": "user", "content": message})
+        request.session['prompts'].append({"role": "user", "content": prompt})
+
+        # define functions for chat completion
+        functions = [
+            {
+                "name": "send_email",
+                "description": "Sends an email to the specified recipients",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "recipients": {
+                            "type": "array",
+                            "items": {
+                                "type": "string",
                             },
-                            "subject": {"type": "string"},
-                            "body": {"type": "string"},
+                            "description": "Email addresses to send the email to",
                         },
-                        "required": ["recipients"],
+                        "subject": {"type": "string"},
+                        "body": {"type": "string"},
                     },
+                    "required": ["recipients"],
                 },
-                {
-                    "name": "generate_image",
-                    "description": "Generates an image based on the prompt",
-                    "parameters": {
-                        "type": "object",
-                        "properties": {},
-                    },
+            },
+            {
+                "name": "generate_image",
+                "description": "Generates an image based on the prompt",
+                "parameters": {
+                    "type": "object",
+                    "properties": {},
                 },
-            ]
+            },
+        ]
 
+        try:
             # Step 1: send the conversation and available functions to GPT
             response = openai.ChatCompletion.create(
                 model="gpt-3.5-turbo",
                 messages=request.session['prompts'],
                 functions=functions,
                 function_call="auto",
+                request_timeout=29,
             )
             response_message = response['choices'][0]['message']
 
@@ -190,18 +191,14 @@ def send_prompt(request):
             request.session['prompts'].append(response_message)
             # save session modifications
             request.session.modified = True
-            return redirect('index')
         
-        else:
-            # if the request is not a POST request, return the home page
-            return redirect('index')
-        
-    except Exception as e:
-        # if there is an error, return error message
-        request.session['messages'].append({"role": "assistant", "content": str(e)})
-        # save session modifications
-        request.session.modified = True
-        return redirect('index')
+        except Exception as e:
+            # if there is an error, return error message
+            request.session['messages'].append({"role": "assistant", "content": str(e)})
+            # save session modifications
+            request.session.modified = True
+    
+    return redirect('index')
 
 def send_email(recipients, subject, body):
     try:
